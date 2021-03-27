e1500dfedd5654cd4203ab59438f847ede4f2fd8
Amanem00 <72438328+amanem1@users.noreply.github.com>
2021-03-27 21:20:09 +0530

    Update esp_network.py

--- esp_network.py
+++ esp_network.py
@@ -1,3 +1,30 @@
-#amannana
-#nnsc
-import pyi
+#first is to establish network for the board 
+import network
+
+wlan = network.WLAN(network.STA_IF) # create station interface
+wlan.active(True)       # activate the interface
+wlan.scan()             # scan for access points
+wlan.isconnected()      # check if the station is connected to an AP
+wlan.connect('essid', 'password') # connect to an AP
+wlan.config('mac')      # get the interface's MAC adddress
+wlan.ifconfig()         # get the interface's IP/netmask/gw/DNS addresses
+
+ap = network.WLAN(network.AP_IF) # create access-point interface
+ap.active(True)         # activate the interface
+ap.config(essid='ESP-AP') # set the ESSID of the access point
+
+#connecting to your local wifi
+def do_connect():
+    import network
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    if not wlan.isconnected():
+        print('connecting to network...')
+        wlan.connect('essid', 'password')
+        while not wlan.isconnected():
+            pass
+    print('network config:', wlan.ifconfig())
+'''    
+this will establish network between esp and local wifi
+well using c++ setup too you can build establish network 
+using 
